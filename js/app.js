a2465a235be5b41515df7f7cf57f8cf2b6a08205
rniochy <lniochy@gmail.com>
2022-09-19 01:25:30 +0100

    adding js features

---
+++ js/app.js
@@ -0,0 +1,4 @@
+const name = document.getElementById('name');
+const date = document.getElementById('date');
+const time = document.getElementById('time');
+const button = document.getElementById('button');
