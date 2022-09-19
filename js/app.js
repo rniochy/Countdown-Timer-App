b4d3d712681630e9967fa9f1826caa5da4db7ecd
rniochy <lniochy@gmail.com>
2022-09-19 06:02:09 +0100

    adding js features bugs to extrate date value in the input HTML componet

--- js/app.js
+++ js/app.js
@@ -8,11 +8,11 @@ const message = document.getElementById('alert-message');
 button.addEventListener('click', function() {
     if(!nameInput.value){
         message.innerText = "The field 'name' is required."
-        setTimeout(()=> message.innerText = "", 2000);
+        setTimeout(()=> message.innerText = "", 3000);
     } 
         alert(dateInput)
     if(new Date(`${dateInput.value}`) < new Date()){
         message.innerText = "The date is not corretly.."
-        setTimeout(()=> message.innerText = "", 2000);
+        setTimeout(()=> message.innerText = "", 3000);
     }
 })
