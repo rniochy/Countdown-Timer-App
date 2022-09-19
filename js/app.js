bfccc7803aa6dc35473d9e8c4f59dfb64762a037
rniochy <lniochy@gmail.com>
2022-09-19 01:38:20 +0100

    adding js features

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
-const name = document.getElementById('name');
-const date = document.getElementById('date');
-const time = document.getElementById('time');
+const nameInput = document.getElementsByClassName('name');
+const date = document.getElementsByClassName('date');
+const time = document.getElementsByClassName('time');
 const button = document.getElementById('button');
+const message = document.getElementById('alert-message');
+
+
+button.addEventListener('click', function() {
+    
+    if(nameInput){
+        message.innerText = "The field 'name' is required.";
+    }
+})
