cd1f307a2dff077216d6991aa8cbb4be62cde986
rniochy <lniochy@gmail.com>
2022-09-21 12:38:36 +0100

    added new features

--- js/app.js
+++ js/app.js
@@ -5,6 +5,12 @@ const displaySchedule = document.getElementById('schedule');
 const button = document.getElementById('button');
 const message = document.getElementById('alert-message');
 
+function display(dayCounter, hours,minutes, seconds ){
+    return `
+    <div id="schedule" class="schedule div-container"> 
+    <h2>${dayCounter} - ${hours} - ${minutes} - ${seconds < 10 ? '0'+seconds: seconds}</h2>;
+    </div> `
+}
 
 button.addEventListener('click', function() {
     if(!nameInput.value){
@@ -13,42 +19,48 @@ button.addEventListener('click', function() {
     } 
 
      const date = new Date(dateInput.value+""); 
+     console.log(date > new Date())
+    if((date > new Date()) && (date.getFullYear() < 2025)){
 
-    if((date < new Date() && date.getDate()) && (date.getFullYear() > 2024)){
+        const curent_date = new Date();
+        let year = date.getFullYear() - curent_date.getFullYear();
+        let month = (date.getMonth()+1) - (curent_date.getMonth()+1); 
+        let day = Math.abs(date.getDate() -  curent_date.getDate());  
+        let minutesCurrent = curent_date.getMinutes();
+
+        if(month < 0) month = 12 - Math.abs(month);
+
+        let dayCounter = (year * 365) + (month * 30) + day;
+        let hours = (dayCounter * 24) - curent_date.getHours();
+        let minutes = hours * 60;
+        let seconds = 59 - curent_date.getSeconds();
+        
+        minutes = (minutes - (24 - hours)) - minutesCurrent;
+        dayCounter = Math.floor((minutes/24)/60);
+
+        const timer = setInterval(()=>{ 
+           displaySchedule.innerHTML = display(dayCounter, hours,minutes, seconds );
+           
+           if(seconds ===0 ){
+               seconds = 59;
+               minutes--;
+
+            }else if((((hours*60)) - hours) === minutes && hours > 0){
+                 hours--;
+
+            }else if((dayCounter * 24) === hours && hours > 0)  {
+                    dayCounter--;
+
+             } else if(dayCounter === 0 && hours === 0 && minutes === 0){
+                    clearInterval(timer);
+                    displaySchedule.innerHTML =display(dayCounter, hours,minutes, seconds );
+             }
+             else {
+                seconds--;
+            }
+        }, 1000);
+    } else {
         message.innerText = "The date is not corretly.."
         setTimeout(()=> message.innerText = "", 3000);
-    } else {
-             const curent_date = new Date();
-             let year = date.getFullYear() - curent_date.getFullYear();
-             let month = (date.getMonth()+1) - (curent_date.getMonth()+1); 
-             let day = Math.abs(date.getDate() -  curent_date.getDate());  
-             let minutesCurrent = curent_date.getMinutes();
-
-             if(month < 0) month = 12 - Math.abs(month);
-
-             let dayCounter = (year * 365) + (month * 30) + day;
-             let hours = (dayCounter * 24) - curent_date.getHours();
-             let minutes = hours * 60;
-             let seconds = 59 - curent_date.getSeconds();
-             
-             minutes = (minutes - (24 - hours)) - minutesCurrent;
-             dayCounter = Math.floor((minutes/24)/60);
-
-             setInterval(()=>{ 
-                 
-
-                displaySchedule.innerHTML = `<h2>${hours} ${dayCounter} ${minutes} ${seconds}</h2>`;
-
-                if(seconds ===0 ){
-                    seconds = 59;
-                    minutes--;
-                }else if(minutes === 0){
-                     hours--;
-                } else if(hours === 0){
-                        dayCounter--;
-                }else {
-                    seconds--;
-                }
-                }, 1000);
     }
 })
